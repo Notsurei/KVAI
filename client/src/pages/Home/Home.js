8f9560ec315d1fe594233fe438dbd33b9ed38634
unknown <galaxylol305@gmail.com>
2024-10-30 12:00:54 +0700

    first commit

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -29,7 +29,7 @@ export default function Home() {
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
             <div className="hidden sm:mb-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-                Announcing Masync AI Content Generator full release{" "}
+                Announcing KVAI Content Generator full release{" "}
                 <Link to="#" className="font-semibold text-white">
                   <span className="absolute inset-0" aria-hidden="true" />
                   Read more <span aria-hidden="true">&rarr;</span>
