e41c1c613154828902a6914cbccf42bb34ff89d8
Notsurei <115835703+Notsurei@users.noreply.github.com>
2024-11-05 14:40:30 +0700

    Update Register.js

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 import {toast} from 'react-toastify';
 import {useNavigate, Link} from 'react-router-dom';
 import { useAuth } from '../../Context/AuthContext';
